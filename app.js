31e49a0b96bc6b63dd21e21fc03f54db44e9eeb8
mohammadzohaib23 <mohammadzohaib2398@gmail.com>
2023-09-07 18:14:44 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,185 @@
+// Chapter NO 1
+
+// Q1
+//  Part 1
+var firstName = "Muhammad Zohaib";
+alert("First Name : " + firstName);
+
+//  Part 2
+
+var lastName = "Jahanzaib";
+alert("Last Name : " + lastName);
+
+//  Part 3
+
+var email = "mohammadzohaib2398@gmail.com"
+alert("Email : "  + email);
+
+//  Part 4
+
+var phoneNumber = "03182146036";
+alert("Phone Number : " + phoneNumber);
+
+//  Part 5
+
+var password = "*********"
+alert("Password : "  + password);
+
+// Q2
+
+alert("You're learning JavaScript!");
+
+// Q3
+
+alert("Hello, world! This is a custom alert message.");
+
+
+// Chapter NO 2
+
+// Q1
+
+var myVariableName = "ThisIsMyVariable";
+
+// Q2
+
+var myVariable;
+myVariable = "This is a string value.";
+
+// Q3
+
+var teamName = "MyTeam";
+alert("Team Name: " + teamName);
+
+// Q4
+
+var bestMan = "Charlie";
+bestMan = "David";
+
+
+// Chapter NO 3
+
+// Q1
+
+var caseQty; 
+
+
+
+// Q2
+
+var caseQty; 
+caseQty = 144; 
+
+
+// Q3
+
+var num = 9; 
+
+
+// Q4
+
+var result;
+result = 5 + 7;
+
+// Q5
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+console.log(orderTotal);
+
+
+
+// Q6
+
+var myNumber = 5; 
+myNumber = myNumber + 3; 
+
+console.log(myNumber); 
+
+// Chapter NO 4
+
+// Q1
+
+var productCost = 3.45; // Variable names cannot contain spaces.
+
+
+// Q2
+
+var nameOfBand;
+
+// Q3
+
+
+var myNumber = 42;
+
+
+// Q4
+
+var firstName = "John";
+var lastName = "Doe";
+var fullName = firstName + lastName; 
+
+
+// Q5
+
+Legal Variable Names:
+
+myVar
+name
+_underscoreVar
+$dollarVar
+camelCaseVar
+firstName
+lastName
+fullName
+Illegal Variable Names:
+
+3rdPlace (Variable names cannot start with a number)
+product cost (Variable names cannot contain spaces)
+my-variable (Variable names cannot contain hyphens)
+alert (Reserved keyword in JavaScript)
+for (Reserved keyword in JavaScript)
+var (Reserved keyword in JavaScript)
+
+
+// Chapter NO 6
+
+// Q1
+
+The name of the arithmetic operator that gives you the remainder when one number is divided by another is called the "modulo" operator. Its symbol is %.
+
+
+
+// Q2
+
+20 % 6 = 2
+
+
+
+// Q3
+
+var largeNum = 1000 * 2000;
+
+// Q4
+
+var num1 = 10;
+var num2 = 5;
+var result = num1 - num2;
+
+
+// Q5
+
+var myRemainder = 15 % 7;
+
+
+// Q6
+
+var num1 = 8;
+var num2 = 4;
+var multiplicationResult = num1 * num2;
+
+alert("The result of the multiplication is: " + multiplicationResult);
+
+
+
